90e580362a07679b6f17d42fe6c0221455895692
zijingng <zijingng@gmail.com>
2024-01-04 11:05:21 +0800

    1.0.0

--- editor-enhancements.ts
+++ editor-enhancements.ts
@@ -60,21 +60,4 @@ export class EditorExtensions {
       end: cursor,
     };
   }
-
-  public static getEditorPositionFromIndex(
-    content: string,
-    index: number
-  ): EditorPosition {
-    let substr = content.substr(0, index);
-
-    let l = 0;
-    let offset = -1;
-    let r = -1;
-    for (; (r = substr.indexOf("\n", r + 1)) !== -1; l++, offset = r);
-    offset += 1;
-
-    let ch = content.substr(offset, index - offset).length;
-
-    return { line: l, ch: ch };
-  }
 }
--- main.ts
+++ main.ts
@@ -1,81 +1,115 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EditorExtensions } from "editor-enhancements";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PlayerPluginSettings {
+	enhanceDefaultPaste: boolean;
+	enableAutoPlay: boolean;
+	enableDanmaku: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface PasteFunction {
+	(this: HTMLElement, ev: ClipboardEvent): void;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: PlayerPluginSettings = {
+	enhanceDefaultPaste: true,
+	enableAutoPlay: false,
+	enableDanmaku: true
+}
+
+export default class PlayerPlugin extends Plugin {
+	settings: PlayerPluginSettings;
+	pasteFunction: PasteFunction;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'convert-bilibili-url',
+			name: 'Convert Bilibili URL to Embedded Player',
+			editorCallback: (editor) => this.convertUrlToPlayer(editor),
+			hotkeys: [
+				{
+				modifiers: ["Mod", "Shift"],
+				key: "b",
+				},
+			],
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.pasteFunction = this.pasteUrlToPlayer.bind(this);
+		this.registerEvent(
+			this.app.workspace.on("editor-paste", this.pasteFunction)
+		);
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new PlayerSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+	}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	convertUrlToPlayer(editor: Editor): void {
+		let selectedText = (EditorExtensions.getSelectedText(editor) || "").trim();
+		console.log(selectedText);
+
+		let bvidRegex = /(?<=\/video\/)[a-zA-z0-9]+/gi;
+		let match = selectedText.match(bvidRegex);
+		if (match === null) return;
+		console.log(match[0]);
+		let bvid = match[0];
+
+		let p = "1";
+		let pRegex = /(?<=\?p\=)[0-9]+/gi;
+		match = selectedText.match(pRegex);
+		if (match !== null) p = match[0];
+		console.log(p);
+
+		let danmaku = this.settings.enableDanmaku ? "1" : "0";
+		let autoplay = this.settings.enableAutoPlay ? "1" : "0";
+
+		let playerText = `<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;"><iframe src="https://player.bilibili.com/player.html?bvid=${bvid}&p=${p}&autoplay=${autoplay}&danmaku=${danmaku}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> </iframe></div>\n`;
+		
+		editor.replaceSelection(playerText);
+		//editor.
+	}
+	
+	async pasteUrlToPlayer(clipboard: ClipboardEvent, editor: Editor): Promise<void> {
+		if (!this.settings.enhanceDefaultPaste) {
+			return;
+		}
+	
+		let clipboardText = clipboard.clipboardData.getData("text/plain");
+		if (clipboardText === null || clipboardText === "") return;
+		
+		let lineRegex = /(https?:\/\/(?:www\.|(?!www))bilibili\.[^\s]{2,}|www\.bilibili\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))bilibili\.[^\s]{2,}|www\.bilibili\.[^\s]{2,})/gi
+		let urlsInLine = clipboardText.match(lineRegex);
+		if (urlsInLine === null) return;
+
+		let bvidRegex = /(?<=\/video\/)[a-zA-z0-9]+/gi;
+		let match = clipboardText.match(bvidRegex);
+		if (match === null) return;
+		console.log(match[0]);
+		let bvid = match[0];
+
+		// We've decided to handle the paste, stop propagation to the default handler.
+		clipboard.stopPropagation();
+		clipboard.preventDefault();
+
+		let p = "1";
+		let pRegex = /(?<=\?p\=)[0-9]+/gi;
+		match = clipboardText.match(pRegex);
+		if (match !== null) p = match[0];
+		console.log(p);
+
+		let danmaku = this.settings.enableDanmaku ? "1" : "0";
+		let autoplay = this.settings.enableAutoPlay ? "1" : "0";
+
+		let playerText = `<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;"><iframe src="https://player.bilibili.com/player.html?bvid=${bvid}&p=${p}&autoplay=${autoplay}&danmaku=${danmaku}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> </iframe></div>\n`;
+		
+		editor.replaceSelection(playerText);
+		return;
 	}
 
 	onunload() {
@@ -91,26 +125,11 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class PlayerSettingTab extends PluginSettingTab {
+	plugin: PlayerPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PlayerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +140,39 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('开启粘贴强化')
+			.setDesc('支持将复制的视频链接转换为嵌入式播放器')
+			.addToggle((val) =>
+				val
+				.setValue(this.plugin.settings.enhanceDefaultPaste)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+				console.log(value);
+				this.plugin.settings.enhanceDefaultPaste = value;
+				await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+		.setName('开启弹幕')
+		.setDesc('开启嵌入式播放器的弹幕功能')
+		.addToggle((val) =>
+			val
+			.setValue(this.plugin.settings.enableDanmaku)
+			.onChange(async (value) => {
+			console.log(value);
+			this.plugin.settings.enableDanmaku = value;
+			await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('开启自动播放')
+		.setDesc('开启嵌入式播放器的自动播放功能')
+		.addToggle((val) =>
+			val
+			.setValue(this.plugin.settings.enableAutoPlay)
+			.onChange(async (value) => {
+			console.log(value);
+			this.plugin.settings.enableAutoPlay = value;
+			await this.plugin.saveSettings();
+			}));
 	}
 }
